9ed67669e0a2d9d1679c1f8583e6a88156c12b7b
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-07-19 06:34:40 +0200

    fix ffmpeg command

--- src/ffmpeg_utils.rs
+++ src/ffmpeg_utils.rs
@@ -2,8 +2,8 @@ use tokio::process::Command;
 
 pub async fn create_thumbnail(video_path: String, thumbnail_path: String) -> Result<(), Box<dyn std::error::Error>> {
     //println!("IN create_thumbnail with: {}, {}", video_path, thumbnail_path);
-    let command = format!("ffmpeg -i {} -ss 00:00:00 -vframes 1 {} -y", video_path, thumbnail_path);
-    let child = Command::new(command).spawn();
+    let args = format!(" -i {} -ss 00:00:00 -vframes 1 {} -y", video_path, thumbnail_path);
+    let child = Command::new("ffmpeg").arg("args").spawn();
     let future = child.expect("failed to spawn");
     let status = future.await?;
     //println!("status: {}", status );
