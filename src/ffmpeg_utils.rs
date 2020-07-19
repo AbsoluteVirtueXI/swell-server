bfc49166d6b297d8ab634751aaf6ca1c562507d8
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-07-19 06:46:56 +0200

    comment

--- src/ffmpeg_utils.rs
+++ src/ffmpeg_utils.rs
@@ -3,7 +3,7 @@ use std::borrow::Borrow;
 
 pub async fn create_thumbnail(video_path: String, thumbnail_path: String) -> Result<(), Box<dyn std::error::Error>> {
     //println!("IN create_thumbnail with: {}, {}", video_path, thumbnail_path);
-    let args = format!(" -i {} -ss 00:00:00 -vframes 1 {} -y", video_path, thumbnail_path);
+    //let args = format!(" -i {} -ss 00:00:00 -vframes 1 {} -y", video_path, thumbnail_path);
     let child = Command::new("ffmpeg")
         .arg("-i")
         .arg(video_path)
